1207b721f9efbfc1a36043aa7b68842393ecfb9d
Your Name <you@example.com>
2026-02-06 15:31:45 -0300

    ajuste de banco padrao para 8k e nao 16k

--- fragmentation.sql
+++ fragmentation.sql
@@ -6,9 +6,9 @@ col FRAGMENTED_SPACE for a40
 col owner for a25
 
 select
-    owner, table_name,avg_row_len,round(((blocks*16/1024)),2)||'MB' "TOTAL_SIZE",
+    owner, table_name,avg_row_len,round(((blocks*8/1024)),2)||'MB' "TOTAL_SIZE",
     round((num_rows*avg_row_len/1024/1024),2)||'Mb' "ACTUAL_SIZE",
-    round(((blocks*16/1024)-(num_rows*avg_row_len/1024/1024)),2) ||'MB' "FRAGMENTED_SPACE",
-    (round(((blocks*16/1024)-(num_rows*avg_row_len/1024/1024)),2)/round(((blocks*16/1024)),2))*100 "percentage"
+    round(((blocks*8/1024)-(num_rows*avg_row_len/1024/1024)),2) ||'MB' "FRAGMENTED_SPACE",
+    (round(((blocks*8/1024)-(num_rows*avg_row_len/1024/1024)),2)/round(((blocks*8/1024)),2))*100 "percentage"
 from dba_tables
 where owner not like '%SYS%' and blocks > (1024*8) order by 7;
